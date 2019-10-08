c51bd47334dc74aedf1e098fba0729bd74ef2b51
Cameron Sanchez <cameron.sanchez@byu.edu>
2019-10-08 09:39:32 -0600

    make header and footer responsive

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <v-app-bar app color="primary" height="40">
       <v-app-bar-nav-icon color="secondary"></v-app-bar-nav-icon>
-      <v-btn color="secondary" icon>
+      <v-btn color="secondary" icon v-if="$vuetify.breakpoint.smAndUp">
         <v-icon>mdi-home</v-icon>
       </v-btn>
       <v-spacer></v-spacer>
@@ -17,12 +17,14 @@
         single-line
       ></v-text-field>
       <v-spacer></v-spacer>
-      <v-btn color="secondary" icon>
-        <v-icon>mdi-cart</v-icon>
-      </v-btn>
-      <v-btn color="secondary" icon>
-        <v-icon>mdi-account</v-icon>
-      </v-btn>
+      <div v-if="$vuetify.breakpoint.smAndUp">
+        <v-btn color="secondary" icon>
+          <v-icon>mdi-cart</v-icon>
+        </v-btn>
+        <v-btn color="secondary" icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </div>
     </v-app-bar>
 
     <v-content>
@@ -30,19 +32,39 @@
     </v-content>
 
     <v-footer app color="primary" height="40">
-      <v-img alt="BYU Library logo" height="20" max-width="216" src="@/assets/logo.svg"></v-img>
+      <v-img
+        alt="BYU Library logo"
+        height="20"
+        max-width="216"
+        src="@/assets/logo.svg"
+        v-if="$vuetify.breakpoint.mdAdndUp"
+      ></v-img>
+      <v-img
+        alt="BYU Library logo"
+        height="15"
+        max-width="162"
+        src="@/assets/logo.svg"
+        v-else
+      ></v-img>
       <v-spacer></v-spacer>
-      <router-link class="secondary--text" to="/hours">Hours</router-link>
-      <span class="secondary--text">&nbsp;|&nbsp;</span>
-      <router-link class="secondary--text" to="/location">Location</router-link>
-      <span class="secondary--text">&nbsp;|&nbsp;</span>
-      <router-link class="secondary--text" to="/contact">Contact</router-link>
-      <span class="secondary--text">&nbsp;|&nbsp;</span>
-      <router-link class="secondary--text" to="/blog">Blog</router-link>
-      <span class="secondary--text">&nbsp;|&nbsp;</span>
-      <router-link class="secondary--text" to="/social">Social</router-link>
+      <div v-if="$vuetify.breakpoint.smAndUp">
+        <router-link class="secondary--text" to="/hours">Hours</router-link>
+        <span class="secondary--text">&nbsp;|&nbsp;</span>
+        <router-link class="secondary--text" to="/location">Location</router-link>
+        <span class="secondary--text">&nbsp;|&nbsp;</span>
+        <router-link class="secondary--text" to="/contact">Contact</router-link>
+        <span class="secondary--text">&nbsp;|&nbsp;</span>
+        <router-link class="secondary--text" to="/blog">Blog</router-link>
+        <span class="secondary--text">&nbsp;|&nbsp;</span>
+        <router-link class="secondary--text" to="/social">Social</router-link>
+      </div>
       <v-spacer></v-spacer>
-      <span class="secondary--text">© 2019 Brigham Young University</span>
+      <span class="secondary--text" v-if="$vuetify.breakpoint.smAndUp">
+        © 2019 Brigham Young University
+      </span>
+      <span class="secondary--text" v-else>
+        © 2019 BYU
+      </span>
     </v-footer>
   </v-app>
 </template>
